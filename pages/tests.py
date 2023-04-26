d5deedb95ad36925c2ea89d804ba073fd5951194
Brayan-A <brayanayol55@gmail.com>
2023-04-26 09:51:24 -0500

    feat: pages app

---
+++ pages/tests.py
@@ -0,0 +1,26 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from .views import HomePageView
+
+
+class HomePageTests(SimpleTestCase):
+    def setUp(self) -> None:
+        url = reverse("home")
+        self.response = self.client.get(url)
+
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, "home.html")
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, "homepage")
+
+    def test_homepage_does_not_contains_incorrect(self):
+        self.assertNotContains(self.response, "error")
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
