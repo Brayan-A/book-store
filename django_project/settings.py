1e7fcbe203e38bcbe351b41a70091134e478b551
Brayan-A <brayanayol55@gmail.com>
2023-05-07 09:22:17 -0500

    feat: confirmation email

--- django_project/settings.py
+++ django_project/settings.py
@@ -172,3 +172,7 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
+
+# Email
+DEFAULT_FROM_EMAIL = "admin@cocobookstore.com"
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
